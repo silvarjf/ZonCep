c8e5320658eff43c680b744b0c8271b50001a99e
Silvano Filho <silvarjf@gmail.com>
2017-07-07 12:40:59 -0300

    Movendo class balancoHidrico.py

--- execSimul.py
+++ execSimul.py
@@ -4,7 +4,7 @@ import sqlite3
 
 from pandas import DataFrame
 
-from balancoHidrico import balancoHidrico
+from calculos.balancoHidrico import balancoHidrico
 from calculos.cultura import Cultura
 from calculos.estacao import Estacao
 from calculos.estrutura import ParamSimul
